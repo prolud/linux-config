3d4cd91b5dc5091996faeb291f48e5cdfd036a8c
Lucas Aguiar <7lucasdaniel@gmail.com>
2026-07-17 15:19:28 -0300

    add sourcegit installation file

---
+++ packages/sourcegit.sh
@@ -0,0 +1,5 @@
+sudo mkdir -p /etc/apt/keyrings
+curl https://codeberg.org/api/packages/yataro/debian/repository.key | sudo tee /etc/apt/keyrings/sourcegit.asc
+echo "deb [signed-by=/etc/apt/keyrings/sourcegit.asc, arch=amd64,arm64] https://codeberg.org/api/packages/yataro/debian generic main" | sudo tee /etc/apt/sources.list.d/sourcegit.list
+sudo apt update
+sudo apt install sourcegit
