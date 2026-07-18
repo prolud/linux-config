5688ecf8dbf2aafcc4f271a047d0088f3a3ad3e4
Lucas Aguiar <7lucasdaniel@gmail.com>
2026-07-17 21:57:14 -0300

    feat: remove unwanted copies

--- packages/files.sh
+++ packages/files.sh
@@ -1,5 +1,2 @@
-cp -f ./config/.p10k.zsh ~/.p10k.zsh
 cp -r ./pictures/me ~/Pictures/
 cp -r ./pictures/wallpapers ~/Pictures/
-
-gsettings set org.cinnamon.desktop.background picture-uri "file:///home/lucas/Pictures/wallpapers/pc-retro.png"
