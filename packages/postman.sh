754267347a203bbbd469e1ca9b856612c010d8bc
Lucas Aguiar <7lucasdaniel@gmail.com>
2026-07-27 14:42:49 -0300

    feat: add postman installation file

---
+++ packages/postman.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/bash
+
+# [DESCRIPTION]
+# this file is responsable for postman installation
+# this method uses flatpak and its not the recommended by postman installation guide
+
+# [STATUS]
+# OK!
+
+flatpak install flathub com.getpostman.Postman
