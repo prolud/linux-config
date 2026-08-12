9107a4ab64589d875695b401a870ca7b08f9c5cb
Lucas Aguiar <7lucasdaniel@gmail.com>
2026-08-12 16:33:24 -0300

    feat: add dbeaver installation file

---
+++ packages/dbeaver.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/bash
+
+# [DESCRIPTION]
+# this file is responsable for dbeaver installation
+# it installs dbeaver by the official apt source
+
+# [STATUS]
+# OK!
+
+set -euo pipefail
+
+sudo wget -q -O - https://dbeaver.io/debs/dbeaver.gpg.key | sudo gpg --dearmor -o /usr/share/keyrings/dbeaver.gpg.key
+echo "deb [signed-by=/usr/share/keyrings/dbeaver.gpg.key] https://dbeaver.io/debs/dbeaver-ce /" | sudo tee /etc/apt/sources.list.d/dbeaver.list
+sudo apt-get update && sudo apt-get install dbeaver-ce
