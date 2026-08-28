0df368e0b7b3b1bbda4e6e9ed11c3bf666cef151
Lucas Aguiar <7lucasdaniel@gmail.com>
2026-08-28 10:20:37 -0300

    feat: add vlc installation file

---
+++ packages/vlc.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/bash
+
+# [DESCRIPTION]
+# this file is responsable for postman installation
+# this method uses flatpak to install vlc media player
+
+# [STATUS]
+# OK!
+
+flatpak install flathub org.videolan.VLC -y
